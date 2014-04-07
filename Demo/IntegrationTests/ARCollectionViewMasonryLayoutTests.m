c40e5b150c255a76089fa4003d778ce6a664b337
Ash Furrow <ash@ashfurrow.com>
2014-04-07 16:05:18 +0200

    Moved things around.

---
+++ Demo/IntegrationTests/ARCollectionViewMasonryLayoutTests.m
@@ -0,0 +1,17 @@
+//
+//  ARCollectionViewMasonryLayoutTests.m
+//  Demo
+//
+//  Created by Ash Furrow on 2014-04-07.
+//  Copyright (c) 2014 Artsy. All rights reserved.
+//
+
+#define EXP_SHORTHAND
+#import <Specta/Specta.h>
+//#import <Specta/Specta.h>
+//#import <Expecta/Expecta.h>
+//#import <EXPMatchers+FBSnapshotTest/EXPMatchers+FBSnapshotTest.h>
+//
+//SpecBegin(ARCollectionViewMasonryLayoutTests)
+//
+//SpecEnd
